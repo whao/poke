982c898804914b4c317e3836fc478a488d1859f3
Wei Hao <dolem.hao@gmail.com>
2023-03-24 02:12:54 +0800

    Complete level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String topBanner = "################################\n";
+        String bottomBanner = "################################\n";
+        String welcomeMsg = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(topBanner);
+        System.out.println(welcomeMsg);
+        System.out.println(bottomBanner);
+        String userInput = new Scanner(System.in).nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(topBanner);
+            System.out.println(userInput);
+            System.out.println(bottomBanner);
+            userInput = new Scanner(System.in).nextLine();
+        }
+        System.out.println(topBanner);
+        System.out.println("Bye.");
+        System.out.println(bottomBanner);
     }
 }
